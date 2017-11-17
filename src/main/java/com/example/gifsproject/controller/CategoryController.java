3a91248b76c1a1b75edbd8418fc63399a84b0828
Artur Faruga <farugaa.training@gmail.com>
2017-11-17 11:37:23 +0100

    P1ZAD3 add logs to category

--- src/main/java/com/example/gifsproject/controller/CategoryController.java
+++ src/main/java/com/example/gifsproject/controller/CategoryController.java
@@ -1,6 +1,8 @@
 package com.example.gifsproject.controller;
 
 import com.example.gifsproject.repository.CategoryRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -9,11 +11,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CategoryController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(CategoryController.class);
+
+
     @Autowired
     private CategoryRepository categoryRepository;
 
     @GetMapping("/categories")
     public String showCategories(ModelMap modelMap){
+        LOG.info("showCategories");
         modelMap.addAttribute("categories", categoryRepository.getAll());
         return "categories";
     }
