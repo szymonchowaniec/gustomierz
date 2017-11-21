84015d3d87fe2e9be826e026723370708153ddd9
Justyna-TOSHIBA\Justyna <szymonchowaniec@gmail.com>
2017-11-21 21:08:37 +0100

    add member controller

--- src/main/java/com/example/gifsproject/Service/MemberService.java
+++ src/main/java/com/example/gifsproject/Service/MemberService.java
@@ -1,4 +1,41 @@
 package com.example.gifsproject.Service;
 
+import com.example.gifsproject.model.Member;
+import com.example.gifsproject.repository.MemberRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+@Service
 public class MemberService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MemberService.class);
+
+    MemberRepository memberRepository;
+
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+
+
+    }
+
+    public Collection<Member> getMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member getMember(Long id){
+        return memberRepository.findOne(id);
+    }
+
+    public void addMember(Member member) {
+        memberRepository.save(member);//adding rows to book table
+    }
+
+    public void removeMember(Long id) {
+        memberRepository.delete(id);
+    }
 }
