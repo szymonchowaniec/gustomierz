7bc68f11c2a387b32bda5f4930778348b0fe0795
Szymon Chowaniec <szymonchowaniec@gmail.com>
2017-11-21 21:22:27 +0100

    add extends to repository

--- src/main/java/com/example/gifsproject/repository/MemberRepository.java
+++ src/main/java/com/example/gifsproject/repository/MemberRepository.java
@@ -1,13 +1,13 @@
 package com.example.gifsproject.repository;
 
 import com.example.gifsproject.model.Member;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Collection;
 
 @Repository
-public interface MemberRepository extends CrudRepository<Member, Long> {
+public interface MemberRepository extends CrudRepository <Member,Long>  {
 
     Collection<Member> findAll();
 
